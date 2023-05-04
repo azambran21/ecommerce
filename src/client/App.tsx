e6904c05f231a674a2638745cdea6068ec2b3a12
Aileen Chan <aileenchany@gmail.com>
2023-05-04 12:51:13 -0400

    Initialized setup for Redux-Toolkit

---
+++ src/client/App.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import AccountPage from './pages/AccountPage';
+import AddressesPage from './pages/AddressesPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrdersPage from './pages/OrdersPage';
+import PaymentMethodPage from './pages/PaymentMethodPage';
+import ProductPage from './pages/ProductPage' 
+import ProductSearch from './pages/ProductSearch';
+import ShoppingCartPage from './pages/ShoppingCartPage';
+import './index.css'
+const App = () =>{
+    const router = createBrowserRouter([
+        {
+          path: "/", 
+          element: <HomePage />
+        },
+        {
+          path: "/account", 
+          element: <AccountPage/>
+        },
+        {
+          path: '/addresses',
+          element: <AddressesPage />
+        },
+        {
+          path: '/checkout',
+          element: <CheckoutPage />
+        },
+        {
+          path: "/orders", 
+          element: <OrdersPage />
+        },
+         {
+          path: "/paymentmethods", 
+          element: <PaymentMethodPage/>
+        },
+        {
+          path: "/product", 
+          element: <ProductPage/>
+        },
+        {
+          path: "/productsearch", 
+          element: <ProductSearch/>
+        },
+        {
+          path: "/shoppingcart", 
+          element: <ShoppingCartPage/>
+        },
+      ])
+      return (
+        <>
+           <RouterProvider router = {router}/>
+        </>
+      )
+}
+
+
+export default App;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,65 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import HomePage from './pages/HomePage';
-import AccountPage from './pages/AccountPage';
-import AddressesPage from './pages/AddressesPage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrdersPage from './pages/OrdersPage';
-import PaymentMethodPage from './pages/PaymentMethodPage';
-import ProductPage from './pages/ProductPage' 
-import ProductSearch from './pages/ProductSearch';
-import ShoppingCartPage from './pages/ShoppingCartPage';
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/", 
-    element: <HomePage />
-  },
-  {
-    path: "/account", 
-    element: <AccountPage/>
-  },
-  {
-    path: '/addresses',
-    element: <AddressesPage />
-  },
-  {
-    path: '/checkout',
-    element: <CheckoutPage />
-  },
-  {
-    path: "/orders", 
-    element: <OrdersPage />
-  },
-   {
-    path: "/paymentmethods", 
-    element: <PaymentMethodPage/>
-  },
-  {
-    path: "/product", 
-    element: <ProductPage/>
-  },
-  {
-    path: "/productsearch", 
-    element: <ProductSearch/>
-  },
-  {
-    path: "/shoppingcart", 
-    element: <ShoppingCartPage/>
-  },
-])
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+root.render(
   <React.StrictMode>
-    <RouterProvider router = {router}/>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+
+/*
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+ */
 
 
 // <BrowserRouter>
