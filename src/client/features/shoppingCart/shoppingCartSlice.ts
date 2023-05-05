9d6db432a72e7c9e0daa6a452580d4e30860a1f1
Aileen Chan <aileenchany@gmail.com>
2023-05-05 12:26:54 -0400

    Initialized ShoppingCart redux state

--- src/client/features/shoppingCart/shoppingCartSlice.ts
+++ src/client/features/shoppingCart/shoppingCartSlice.ts
@@ -4,17 +4,25 @@ import type { RootState } from '../../app/store'
 
 // Define a type for the slice state
 interface ShoppingCartState {
-  information: object,
+  products: Product[],
   total: number
+}
+type Product = {
+  id:number,
+  title: string,
+  price: number,
+  description: string,
+  category: object,
+  images: string[]
 }
 
 // Define the initial state using that type
 const initialState: ShoppingCartState = {
-  information: [],
+  products: [],
   total: 0
 }
 /*
-information: [{
+products: [{
     name: '',
     description: '',
     price: 0.00,
@@ -23,22 +31,35 @@ information: [{
   total: 0
 */
 
+// products.filter((product, i) => product[i].id === payload.id);
+const deleteProduct = (products: Product[], target:number){ 
+  let newProductArray = [];
+  for(let i = 0; i < products.length; i++){
+    if(products[i].id !== target){
+      newProductArray.push(products[i])
+    }
+  }
+  return newProductArray;
+}
+
 export const shoppingCartSlice = createSlice({
   name: 'shoppingCart',
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
-    increment: (state) => {
+    increment: (state = initialState) => {
       state.total += 1
     },
-    decrement: (state) => {
+    decrement: (state = initialState) => {
       state.total -= 1
     },
-    addToCart: (state) => {
-      state.total += 1
+    addToCart: (state = initialState, action: PayloadAction<Product>) => {
+      // let newState = [...state.products]; //[...initialState.products, state]
+      // newState.push(action.payload);
+      state.products.push(action.payload);
     },
-    removeFromCart: (state) => {
-      state.total -= 1
+    removeFromCart: (state = initialState, action: PayloadAction<Product>) => {
+      // state.products = deleteProduct(state.products, action.payload)
     },
   },
 })
@@ -46,6 +67,6 @@ export const shoppingCartSlice = createSlice({
 export const { increment, decrement, addToCart, removeFromCart } = shoppingCartSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectCount = (state: RootState) => state.counter.total
+export const selectShoppingCart = (state: RootState) => state.shoppingCart.products;
 
 export default shoppingCartSlice.reducer
